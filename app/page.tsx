d5677228c7689909240084f88633e4b82513080c
tanyamakkar28 <marketingtanya8@gmail.com>
2026-05-08 15:08:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>Wedshaala</h1>
-      <p>Your app is live 🎉</p>
+      <p>Site is live ✅</p>
     </div>
   );
 }
