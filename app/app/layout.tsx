fb2767368c53011364e74de054176f69f0cb1d7e
tanyamakkar28 <marketingtanya8@gmail.com>
2026-05-08 15:09:12 +0530

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -5,9 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
